bbed867544e33c175f28878a8eb1ac2117c750d7
Korobkin Alexander <korobkin.alex2000@yandex.ru>
2020-05-03 21:21:33 +0300

    update lastInteractionTime when message sended

--- src/servlet/NewMessageServlet.java
+++ src/servlet/NewMessageServlet.java
@@ -9,17 +9,18 @@ import entity.*;
 
 public class NewMessageServlet extends ChatServlet {
     private static final long serialVersionUID = 1L;
-    protected void doPost(HttpServletRequest request, HttpServletResponse
-            response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
         request.setCharacterEncoding("UTF-8");
         String message = (String)request.getParameter("message");
         if (message!=null && !"".equals(message)) {
-            ChatUser author = activeUsers.get((String)
-                    request.getSession().getAttribute("name"));
+            ChatUser author = activeUsers.get((String) request.getSession().getAttribute("name"));
             synchronized (messages) {
                 messages.add(new ChatMessage(message, author,
                         Calendar.getInstance().getTimeInMillis()));
             }
+            author.setLastInteractionTime(Calendar.getInstance().getTimeInMillis());
         }
         response.sendRedirect("/resource/compose_message.htm");
     }
